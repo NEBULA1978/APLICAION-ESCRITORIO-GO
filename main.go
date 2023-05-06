56b0bcd0983217fe6cbe73406583673e53b452a3
NEBULA1978 <rpascualey@gmail.com>
2023-05-06 21:48:41 +0200

    Input para introducir el nombre y saluda añado opciones

--- main.go
+++ main.go
@@ -22,40 +22,8 @@ func main() {
 
 	content := container.NewVBox(input, send, hello)
 
+	// myWindow.SetContent(widget.NewLabel("Hola mundo"))
 	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
 
 }
-
-// /////////////////////
-// ERRO
-
-// package main
-
-// import (
-// 	"fyne.io/fyne/app"
-// 	"fyne.io/fyne/container"
-// 	"fyne.io/fyne/widget"
-// )
-
-// func main() {
-
-// 	myApp := app.New()
-// 	myWindow := myApp.NewWindow("Hello fyne!")
-// 	// Campo de entrada input
-// 	input := widget.NewEntry()
-// 	input.SetPlaceHolder("Ingrese su nombre")
-// 	// Mostramos el input
-// 	hello := widget.NewLabel("")
-// 	// Cuando hagamos clic en boton mostramos
-// 	send := widget.NewButton("Enviar", func() {
-// 		hello.SetText("Hola " + input.Text)
-// 	})
-
-// 	content := container.NewVBox(input, send, hello)
-
-// 	// myWindow.SetContent(widget.NewLabel("Hola mundo"))
-// 	myWindow.SentContent(content)
-// 	myWindow.ShowAndRun()
-
-// }
