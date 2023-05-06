4bfcc3d3be92028d272c25126e7aada862aa4319
NEBULA1978 <rpascualey@gmail.com>
2023-05-06 21:34:50 +0200

    Input para introducir el nombre y saluda

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/container"
 	"fyne.io/fyne/widget"
 )
 
@@ -9,7 +10,52 @@ func main() {
 
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Hello fyne!")
+	// Campo de entrada input
+	input := widget.NewEntry()
+	input.SetPlaceHolder("Ingrese su nombre")
+	// Mostramos el input
+	hello := widget.NewLabel("")
+	// Cuando hagamos clic en boton mostramos
+	send := widget.NewButton("Enviar", func() {
+		hello.SetText("Hola " + input.Text)
+	})
 
-	myWindow.SetContent(widget.NewLabel("Hola mundo"))
+	content := container.NewVBox(input, send, hello)
+
+	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
+
 }
+
+// /////////////////////
+// ERRO
+
+// package main
+
+// import (
+// 	"fyne.io/fyne/app"
+// 	"fyne.io/fyne/container"
+// 	"fyne.io/fyne/widget"
+// )
+
+// func main() {
+
+// 	myApp := app.New()
+// 	myWindow := myApp.NewWindow("Hello fyne!")
+// 	// Campo de entrada input
+// 	input := widget.NewEntry()
+// 	input.SetPlaceHolder("Ingrese su nombre")
+// 	// Mostramos el input
+// 	hello := widget.NewLabel("")
+// 	// Cuando hagamos clic en boton mostramos
+// 	send := widget.NewButton("Enviar", func() {
+// 		hello.SetText("Hola " + input.Text)
+// 	})
+
+// 	content := container.NewVBox(input, send, hello)
+
+// 	// myWindow.SetContent(widget.NewLabel("Hola mundo"))
+// 	myWindow.SentContent(content)
+// 	myWindow.ShowAndRun()
+
+// }
